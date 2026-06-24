55f683fb24b1b56d3ece97069e0caa17c907d39a
Varun Sen <vickysen126@gmail.com>
2026-06-24 13:04:52 +0530

    refactor: remove old flat component (moved to subdirectory)

--- components/DashboardShareButton.js
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import ShareButton from '@/components/ShareButton';
-
-export default function DashboardShareButton({ data }) {
-  return <ShareButton type="daily" data={data} />;
-}
