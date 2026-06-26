62a3531b44e88190d5171ffa821ca03a99f52a6c
Varun Sen <vickysen126@gmail.com>
2026-06-26 13:18:53 +0530

    fix: make trophy gallery smaller with 5 columns on wide screens
    Changed grid from max 3 columns to responsive 2/3/4/5 columns
    so trophies display smaller and fit 5 per row on wide screens.

--- components/trophies/TrophyWall.js
+++ components/trophies/TrophyWall.js
@@ -359,7 +359,7 @@ export default function TrophyWall({ trophies, firmNames }) {
       </div>
 
       {/* Gallery grid */}
-      <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-3 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {trophies.map((t) => (
           <TrophyCard
             key={t.id}
